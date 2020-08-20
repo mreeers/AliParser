94c117ca0bfc2b8b26779601029474899117f221
Ефанов Александр Егорович <efanov_ae@oms.admsurgut.ru>
2020-08-20 17:25:43 +0500

    Добавьте файлы проекта.

---
+++ Work/Aliexpress.cs
@@ -0,0 +1,21 @@
+﻿using Leaf.xNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AliParser.Work
+{
+    public class Aliexpress
+    {
+        public static string GetPage(string url)
+        {
+            HttpRequest request = new HttpRequest();
+
+            string response = request.Get(url).ToString();
+
+            return null;
+        }
+    }
+}
